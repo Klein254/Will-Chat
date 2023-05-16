12af7dd7e30c0fb07fbe001fd935d5ee496056b0
Klein254 <rayobiero62@gmail.com>
2023-05-16 17:30:56 +0300

    logo change

---
+++ app/src/main/java/com/example/medilink/adapters/RecentConversationAdapter.java
@@ -0,0 +1,78 @@
+package com.example.medilink.adapters;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.medilink.databinding.ItemContainerRecentConversationBinding;
+import com.example.medilink.listeners.ConversionListener;
+import com.example.medilink.models.ChatMessage;
+import com.example.medilink.models.User;
+
+import java.util.List;
+
+public class RecentConversationAdapter extends RecyclerView.Adapter<RecentConversationAdapter.ConversationViewHolder>{
+
+    private final List<ChatMessage> chatMessages;
+    private final ConversionListener conversionListener;
+
+    public RecentConversationAdapter(List<ChatMessage> chatMessages, ConversionListener conversionListener) {
+        this.chatMessages = chatMessages;
+        this.conversionListener = conversionListener;
+    }
+
+    @NonNull
+    @Override
+    public ConversationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ConversationViewHolder(
+                ItemContainerRecentConversationBinding.inflate(
+                        LayoutInflater.from(parent.getContext()),
+                        parent,
+                        false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ConversationViewHolder holder, int position) {
+        holder.setData(chatMessages.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    class ConversationViewHolder extends RecyclerView.ViewHolder {
+
+        ItemContainerRecentConversationBinding binding;
+
+        ConversationViewHolder(ItemContainerRecentConversationBinding itemContainerRecentConversationBinding) {
+            super(itemContainerRecentConversationBinding.getRoot());
+            binding = itemContainerRecentConversationBinding;
+        }
+
+        void setData(ChatMessage chatMessage) {
+            binding.imageProfile.setImageBitmap(getConversionImage(chatMessage.conversationImage));
+            binding.txtName.setText(chatMessage.conversationName);
+            binding.txtRecentMessage.setText(chatMessage.message);
+            binding.getRoot().setOnClickListener(v -> {
+                User user = new User();
+                user.id = chatMessage.conversationId;
+                user.name = chatMessage.conversationName;
+                user.image = chatMessage.conversationImage;
+                conversionListener.onConversionClicker(user);
+            });
+        }
+    }
+
+    private Bitmap getConversionImage(String encodedImage) {
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+}
